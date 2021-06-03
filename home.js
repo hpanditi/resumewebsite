d72ae59782d7303444992ed062ce9012f204ab71
hpanditi <59629153+hpanditi@users.noreply.github.com>
2021-06-02 20:27:16 -0400

    Added files for resume website

---
+++ home.js
@@ -0,0 +1,24 @@
+alert("Welcome to my website!");
+
+console.log("JavaScript Website");
+
+console.log("Hello World!");
+
+var person = {
+    firstName: "Hemanth",
+    lastName: "Panditi",
+    age: 25
+};
+
+console.log(person);
+
+
+var today = new Date();
+
+var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+
+document.getElementById("p1").innerHTML = time;
+
+
+
+
